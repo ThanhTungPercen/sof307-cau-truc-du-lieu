d39daaa15e940cbbc49513cde4c238241f418c55
ThanhTungPercen <tungttpk01679@fpt.edu.vn>
2021-01-25 16:38:51 +0700

    test

--- Bai-Tap/02-vong-Lap/javaScript.js
+++ Bai-Tap/02-vong-Lap/javaScript.js
@@ -25,4 +25,80 @@ function showMessage10lan(message,n) {
      }
      return  sum ;
  }
+ // 5. Viết hàm tính tổng từ a tới b 
+ function tinhTongAtoiB(a,b){
+    var sum = 0;
+    for ( var i = a; i <= b; i++) {
+        sum = sum +i;
+    }
+    return  sum ;
+ }
+ // 6. Sử dụng hàm (5) tính toán tổng từ 1 tới 100; tính toán tổng từ 20 tới 76
+ function tinhTongAtoiB(a,b){
+    var sum = 0;
+    for ( var i = a; i <= b; i++) {
+        sum = sum +i;
+    }
+    return  sum ;
+ }
+ // 9. Viết hàm xuất ra n số lẻ nhỏ nhất 
+ function soLeNhoNhat(a,b) {
+    var sum = 0;
+    for (var i = a; i <= b; i++) {
+            if(i % 2 != 0){
+                sum = sum +i;
+            }        
+    }
+    return sum;
+ }
+ function danhSachSoLe(from, num) {
+    var list = new Array();
+    var i = from;
+    while (list.length < num) {
+        if (i % 2 != 0) {
+          list.push(i);
+          
+        }
+        i++;
+    }
+    return list;
+}
+// 11. Viết hàm xuất ra n số lẻ gần nhất bắt đầu từ m. Ví dụ: viết ra 3 số lẻ gần nhất bắt đầu từ 102 => 103; 105; 107
+// 13. Viết hàm kiểm tra một số có phải là số nguyên tố hay không? Biết số nguyên tố là số chia hết cho một và chính nó.
+function checkSNT(num) {
+    for (var i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return ' không phải là số nguyên tố' ;
+        }
+    }
+    return  ' là số nguyên tố';
+}
+// 14. Viết hàm xuất ra n số nguyên tố nhỏ nhất. 
+function listSNT(n) {
+    var danhSachSNT = new Array();
+    var dem = 0;
+    var soHienTai = 2;
 
+    while (danhSachSNT.length < n) {
+        if (checkSNT(soHienTai) == true) {
+            danhSachSNT[dem] = soHienTai;
+            dem++;
+        }
+        soHienTai++;
+    }
+    return danhSachSNT;
+}
+function listSNTFrom(from, n) {
+    var danhSachSNT = new Array();
+    var dem = 0;
+    var soHienTai = from;
+
+    while (danhSachSNT.length < n) {
+        if (checkSNT(soHienTai) == true) {
+            danhSachSNT[dem] = soHienTai;
+            dem++;
+        }
+        soHienTai++;
+    }
+    return danhSachSNT;
+}
--- Bai-Tap/04-de-quy/js.js
+++ Bai-Tap/04-de-quy/js.js
@@ -1,7 +1,16 @@
-function fibonacci(n){
-    var tong = 0;
-
+function tinhTong(n){
+   
+    if(n == 0) {
+        return 0;
+    }
+    return tinhTong(n-1) +n;
+   
 }
-
+function tinhGiaiThua (n) {
+    if(n <= 1) {
+        return 1;
+    }
+    return tinhGiaiThua(n-1)*n;
+}
 
 
